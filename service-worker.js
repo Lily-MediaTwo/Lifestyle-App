17db17c5b58e5037cae925f6b6f7b58e5200beea
Lily-MediaTwo <107086363+Lily-MediaTwo@users.noreply.github.com>
2025-08-09 21:17:09 -0400

    Version 2

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,12 @@
-const CACHE='mfl-cache-v1';
+const CACHE='mfl-v2-cache';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./assets/icons/icon-192.png','./assets/icons/icon-512.png'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim())});
 self.addEventListener('fetch',e=>{
   const url=new URL(e.request.url);
-  if(ASSETS.includes('./'+url.pathname.split('/').pop())){
+  const path='./'+url.pathname.split('/').pop();
+  if(ASSETS.includes(path)){
     e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));return;
   }
-  e.respondWith(fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match(e.request)))
+  e.respondWith(fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp}).catch(()=>caches.match(e.request)));
 });
